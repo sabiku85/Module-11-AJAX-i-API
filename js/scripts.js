7c392ef0afb5c10def61add568854aea58b2931c
sabina <pilkauna@wp.pl>
2017-04-18 23:23:25 +0200

    Random Quote Machine

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
-var url = 'http://api.icndb.com/jokes/random';
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function() {
-  getJoke();
-});
-
-var paragraph = document.getElementById('joke');
-
-function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function() {
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
 }
 
-getJoke();
+function createTweet(input) {
+	if (!input.quoteAuthor.length) {
+		input.quoteAuthor = "Unknown author";
+	}
+	var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(input.quoteText);
+		$('.author').text("Author: " + input.quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
+}
+
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
+});
